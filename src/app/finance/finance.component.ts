8591f9f5ef703ad62012a2336b263a24420f9755
Kene Conde <kene.conde@jupiter.co.ao>
2024-07-30 17:35:47 +0100

    update funtion printPage

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -25,7 +25,7 @@ export class FinanceComponent implements OnInit {
   public stackedAreaChartLegend = true;
   public stackedAreaChartData = [
     {
-      data: [3000, 3200, 3300, 3400, 3500, 3700],
+      data: [5000, 5200, 5400, 5600, 5800, 6000],
       label: 'Mensalidades',
       backgroundColor: 'rgba(75, 192, 192, 0.4)',
       borderColor: 'rgba(75, 192, 192, 1)',
@@ -33,7 +33,7 @@ export class FinanceComponent implements OnInit {
       borderWidth: 1
     },
     {
-      data: [1200, 1300, 1400, 1500, 1600, 1700],
+      data: [2500, 2600, 2700, 2800, 2900, 3000],
       label: 'Doações',
       backgroundColor: 'rgba(54, 162, 235, 0.4)',
       borderColor: 'rgba(54, 162, 235, 1)',
@@ -41,7 +41,7 @@ export class FinanceComponent implements OnInit {
       borderWidth: 1
     },
     {
-      data: [800, 900, 1000, 1100, 1200, 1300],
+      data: [1500, 1600, 1700, 1800, 1900, 2000],
       label: 'Subsídios',
       backgroundColor: 'rgba(255, 159, 64, 0.4)',
       borderColor: 'rgba(255, 159, 64, 1)',
@@ -63,21 +63,21 @@ export class FinanceComponent implements OnInit {
   public stackedBarChartLegend = true;
   public stackedBarChartData = [
     {
-      data: [5000, 5200, 5400, 5600, 5800, 6000],
+      data: [8000, 8200, 8400, 8600, 8800, 9000],
       label: 'Salários',
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1
     },
     {
-      data: [1500, 1600, 1700, 1800, 1900, 2000],
+      data: [2500, 2600, 2700, 2800, 2900, 3000],
       label: 'Infraestrutura',
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
       borderWidth: 1
     },
     {
-      data: [1200, 1300, 1400, 1500, 1600, 1700],
+      data: [2000, 2100, 2200, 2300, 2400, 2500],
       label: 'Pesquisa',
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderColor: 'rgba(75, 192, 192, 1)',
